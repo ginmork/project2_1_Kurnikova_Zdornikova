1f8ed598ffcc92d2d18bcc92b2b7f67270fe3575
Курникова Милана Станиславовна <1899-23@std.pkgh.edu>
2025-09-20 15:11:10 +0300

    №1

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,56 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    print("Введите кол-во строк: ")
+    val rows = readln().toInt()
+    print("Введите кол-во столбцов: ")
+    val cols = readln().toInt()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    if (rows % 1 != 0 || rows == null) {
+        println("Ошибка")
+        return
+    }
+    if (cols % 1 != 0 || cols == null) {
+        println("Ошибка")
+        return
+    }
+
+    if (rows <= 0 || cols <= 0) {
+        println("Число строк и столбцов должно быть положительным")
+        return
+    }
+
+    var array = Array(rows) {IntArray(cols)}
+
+    println("Введите " + (cols * rows) + " трехзначных чисел: ")
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            print("Элемент [$i] [$j]: ")
+            val input = readln().toIntOrNull()
+            if (input == null || input < 100 || input > 999) {
+                println("Ошибка, введите трехзначное число")
+                return
+            }
+            array[i][j] = input;
+        }
+    }
+
+    var countUnicNum = mutableSetOf<Char>()
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            var numbers = array[i][j].toString()
+            for (number in numbers) {
+                countUnicNum.add(number)
+            }
+        }
+    }
+
+    println("Двумерный массив")
+    for (i in 0 until rows) {
+        for (j in 0 until cols) {
+            print("${array[i][j]} ")
+        }
+        println()
+    }
+
+    println("В массиве использовано ${countUnicNum.size} различных цифр")
+
 }
